0ba19fa9d37f515984512225982878764654db25
hyebinpark <hoonywhitesocks@gmail.com>
2025-08-28 14:42:45 +0900

    chore(config): Vite build manualChunks 설정 추가 (code splitting 적용)

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "docs",
+    rollupOptions: {
+      // vite의 모듈 번들러 rollup에 옵션 설정
+      output: {
+        manualChunks: (id) => {
+          if (id.indexOf("node_modules") !== -1) {
+            const module = id.split("node_modules/").pop().split("/")[0];
+            return `vendor-${module}`;
+          }
+        },
+      },
+    },
+  },
 });
